bfbb95221a7d331985aa2d7cd334753cd9000af2
trevisolrt <trevisolfelipe85@gmail.com>
2025-04-24 23:43:40 -0300

    added basic shit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/main-dashboard/main-dashboard.component.spec.ts
@@ -0,0 +1,18 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainDashboardComponent } from './main-dashboard.component';
+
+describe('MainDashboardComponent', () => {
+  let component: MainDashboardComponent;
+  let fixture: ComponentFixture<MainDashboardComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
